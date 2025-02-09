c1bdc571f70ce729360ea6fa7ab0d9bc8aa11373
aqib@51214 <syedaqibsafdar@gmail.com>
2025-02-09 17:23:43 +0500

    Initial Commit

---
+++ Talent-Hunt/Talent_HuntApp.swift
@@ -0,0 +1,17 @@
+//
+//  Talent_HuntApp.swift
+//  Talent-Hunt
+//
+//  Created by MacBook Pro on 09/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct Talent_HuntApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
